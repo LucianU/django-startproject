4f2f514ee1efe400ac0e724d3717b16030ec6dd7
Lucian Ursu <lucian.ursu@gmail.com>
2011-11-24 13:46:22 +0200

    Added a bootstrap.py file used to create a virtual env and install packages

---
+++ django_startproject/project_template/bin/bootstrap.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+"""
+This script is to be used when deploying a project for development
+or production. The general use case is that, after the source is cloned
+with git, this script is run to create the virtual env and install 
+the packages specified in requirements/common.pip and those specific
+to the platform (development or production).
+"""
+
+import os
+import sys
+import subprocess
+
+REPO_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ENV_DIR = 'env'
+REQ_FILES = {
+    'common': 'requirements/common.pip',
+    'dev': 'requirements/development.pip',
+    'prod': 'requirements/production.pip',
+}
+
+def main():
+    if (len(sys.argv) < 2 or (len(sys.argv) == 2
+        and sys.argv[1] not in ['prod', 'dev'])):
+        sys.stdout.write("You need to pass 'prod' or 'dev'"
+                         " to specify the deployment environment.\n")
+        return sys.exit(1)
+
+    if ENV_DIR in os.listdir(REPO_ROOT):
+        sys.stdout.write('Virtual env named %s already exists\n' % ENV_DIR)
+        return sys.exit(1)
+
+    sys.stdout.write('Creating virtual env...\n')
+    subprocess.call([
+        'virtualenv',
+        '--no-site-packages',
+        os.path.join(REPO_ROOT, ENV_DIR)
+    ])
+
+    sys.stdout.write('Installing packages in the %s requirements file...\n'
+                     % REQ_FILES['common'].split('/')[1])
+    subprocess.call([
+        os.path.join(REPO_ROOT, ENV_DIR, 'bin', 'pip'),
+        'install', '-r',
+        os.path.join(REPO_ROOT, REQ_FILES['common'])
+    ])
+
+    platform = sys.argv[1]
+    sys.stdout.write('Installing packages in the %s requirements file...\n'
+                        % REQ_FILES[platform].split('/')[1])
+    subprocess.call([
+        os.path.join(REPO_ROOT, ENV_DIR, 'bin', 'pip'),
+        'install', '-r',
+        os.path.join(REPO_ROOT, REQ_FILES[platform])
+    ])
+
+if __name__ == '__main__':
+    main()
