bdd9ff365017198dc30547e862dcbb67abcd20aa
Lucian Ursu <lucian.ursu@gmail.com>
2011-12-08 19:06:18 +0200

    Removed .pth and activate files writing

--- django_startproject/project_template/bin/bootstrap.py
+++ django_startproject/project_template/bin/bootstrap.py
@@ -26,46 +26,6 @@ REQ_FILES = {
     'prod': 'requirements/production.pip',
 }
 
-def get_project_dir():
-    """
-    Returns the name of the project dir; it's hackish, but I don't
-    see a better way to do this
-    """
-    for d in os.listdir(REPO_ROOT):
-        if os.path.isdir(d) and 'settings' in os.listdir(d):
-            return d
-
-def write_pth_file():
-    """
-    Writes a .pth file that makes the project and apps
-    packages discoverable by Python
-    """
-    env_python_path = os.path.join(REPO_ROOT, ENV_DIR, 'bin/python')
-    site_packages = subprocess.check_output([
-                        env_python_path,
-                        '-c', 
-                        'from distutils.sysconfig import get_python_lib;'
-                        'print get_python_lib(),'
-    ]).strip()
-
-    project_dir = get_project_dir()
-    project_rel_path = os.path.relpath(REPO_ROOT, site_packages)
-    project_abs_path = os.path.abspath(project_dir)
-    apps_rel_path = os.path.relpath(
-                        os.path.join(project_abs_path, 'apps'),
-                        site_packages
-    )
-
-    # writing the .pth file
-    with open(os.path.join(site_packages, '%s.pth' % project_dir), 'w') as f:
-        f.writelines(['%s\n' % project_rel_path, apps_rel_path])
-
-def write_activate_file(platform):
-    with open(os.path.join(REPO_ROOT, ENV_DIR, 'bin/activate'), 'a') as f:
-        f.write('export DJANGO_SETTINGS_MODULE=%s.settings.%s' %
-                (get_project_dir(), SETTINGS_MODULES[platform])
-        )
-
 def main():
     if (len(sys.argv) < 2 or (len(sys.argv) == 2
         and sys.argv[1] not in ['prod', 'dev', 'stag'])):
@@ -100,8 +60,6 @@ def main():
         'install', '-r',
         os.path.join(REPO_ROOT, REQ_FILES[platform])
     ])
-    write_pth_file()
-    write_activate_file(platform)
 
 if __name__ == '__main__':
     main()
