507f9ae073ad26cc8039808afc4ca651730ed16d
Lucian Ursu <lucian.ursu@gmail.com>
2013-01-02 18:49:56 +0200

    Adding the apps directory to PYTHONPATH in manage.py.

--- manage.py
+++ manage.py
@@ -3,6 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
+    # Making sure we can import each app
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.join(PROJECT_ROOT, '{{ project_name }}/apps'))
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE",
                           "{{ project_name }}.settings.development")
 
