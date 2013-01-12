cc4ba57927f48fbd1a76283b3579b49d5b2073dd
Lucian Ursu <lucian.ursu@gmail.com>
2013-01-12 13:29:36 +0200

    Stopped setting DJANGO_SETTINGS_MODULE in wsgi.py, because it's being done in the server config. This also prevents using the production settings accidentally.

--- project_name/wsgi.py
+++ project_name/wsgi.py
@@ -1,7 +1,2 @@
-import os
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      '{{ project_name }}.settings.production')
-
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
